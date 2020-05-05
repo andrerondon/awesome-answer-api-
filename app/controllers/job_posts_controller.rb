726c4cc55f2f5fc5693e2f648c16b93ca4547f82
Hano <hinsul3113@gmail.com>
2020-05-05 10:54:53 -0700

    Add tests for #create action

--- app/controllers/job_posts_controller.rb
+++ app/controllers/job_posts_controller.rb
@@ -2,4 +2,18 @@ class JobPostsController < ApplicationController
     def new 
         @job_post = JobPost.new 
     end
+    def create
+        @job_post = JobPost.new job_post_params
+        if @job_post.save
+            redirect_to job_post_path(@job_post)
+        else 
+            render :new 
+        end
+    end
+
+    private 
+    
+    def job_post_params
+        params.require(:job_post).permit(:title, :description, :min_salary, :max_salary, :company_name, :location)
+    end
 end
--- spec/controllers/job_posts_controller_spec.rb
+++ spec/controllers/job_posts_controller_spec.rb
@@ -20,6 +20,7 @@ RSpec.describe JobPostsController, type: :controller do
         # contains the right rendered template.
         expect(response).to(render_template(:new))
     end
+
     it "sets an instance variable with a new job post" do 
         get(:new)
         # assign(:job_post) - returns the value of an instance 
@@ -32,4 +33,55 @@ RSpec.describe JobPostsController, type: :controller do
         # value is a new instance of the JobPost model 
     end
    end 
+
+   describe "#create" do 
+    # 'context' is funcationally the same as 'describe', but
+    # we use context to group branching code paths. 
+    context "with valid parameters" do 
+        def valid_request 
+            # The post method below simulates an HTTP request to the 
+            # create action of JobPostsController using the POST verb 
+            # This has the effect of a user filling out our new form 
+            # in a browser and submitting
+            post(:create, params: { job_post: FactoryBot.attributes_for(:job_post) })
+        end
+
+        it "creates a job post in the db" do 
+            count_before = JobPost.count
+            valid_request
+            count_after = JobPost.count 
+            expect(count_after).to(eq(count_before + 1))
+        end
+
+        it "redirects us to the show page for that job post" do 
+            valid_request
+            job_post = JobPost.last 
+            expect(response).to(redirect_to(job_post_url(job_post.id)))
+        end
+    end
+
+    context "with invalid parameters" do 
+        def invalid_request 
+            post(:create, params: { job_post: FactoryBot.attributes_for(:job_post, title: nil) })
+        end
+
+        it "doesn't save a job post in the db" do
+            count_before = JobPost.count 
+            invalid_request 
+            count_after = JobPost.count 
+            expect(count_after).to eq(count_before)
+        end
+
+        it "renders the new template" do 
+            invalid_request
+            expect(response).to render_template(:new)
+        end
+
+        it "assigns an invalid job_post as an instance variable" do 
+            invalid_request
+            expect(assigns(:job_post)).to be_a(JobPost)
+            expect(assigns(:job_post).valid?).to be(false)
+        end
+    end
+   end
 end
