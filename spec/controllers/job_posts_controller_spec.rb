8eb612d28736e93d81dfa4135d51df3fa4bf8549
Hano <hinsul3113@gmail.com>
2020-05-06 10:45:33 -0700

    Add auth tests for #destroy action JobPosts controller

--- spec/controllers/job_posts_controller_spec.rb
+++ spec/controllers/job_posts_controller_spec.rb
@@ -145,24 +145,56 @@ RSpec.describe JobPostsController, type: :controller do
     end
    end
 
-   describe "#destroy" do 
+   describe "#destroy" do
+
+    context "without signed in user" do 
+        it "redirect to new session page" do 
+            job_post = FactoryBot.create(:job_post)
+            delete(:destroy, params: { id: job_post.id })
+            expect(response).to redirect_to(new_session_path)
+        end
+    end
+
+    context "with signed in user" do 
+        before do 
+            session[:user_id] = current_user.id 
+        end
+
+        context "as owner" do
+            before do 
+                @job_post = FactoryBot.create(:job_post, user: current_user)
+                delete(:destroy, params: { id: @job_post.id })
+            end
+            it "removes a job post from the db" do 
+                expect(JobPost.find_by(id: @job_post.id)).to be(nil)
+            end
+        
+            it "redirects to the job post index" do
+                expect(response).to redirect_to(job_posts_url)
+            end 
+        end
+
+        context "a non owner" do 
+            before do 
+                @job_post = FactoryBot.create(:job_post)
+                delete(:destroy, params: { id: @job_post.id })
+            end
+
+            it "doesn't remove the job post" do   
+                expect(JobPost.find(@job_post.id)).to eq(@job_post)
+            end
+
+            it "redirects to job post show" do 
+                expect(response).to redirect_to(job_post_url(@job_post.id))
+            end
+
+            it "flashes a danger message" do 
+                expect(flash[:danger]).to be 
+            end
+        end
     
-    before do 
-        @job_post = FactoryBot.create(:job_post)
-        delete(:destroy, params: { id: @job_post.id })
     end
-
-    it "removes a job post from the db" do 
-        expect(JobPost.find_by(id: @job_post.id)).to be(nil)
-    end
-
-    it "redirects to the job post index" do
-        expect(response).to redirect_to(job_posts_url)
-    end 
-
-    it "sets a flash message" do 
-        expect(flash[:danger]).to be 
-    end
+    
    end
 
    describe "#index" do 
