07b9ed66955be517c639e367d20d804106af050b
Hano <hinsul3113@gmail.com>
2020-05-06 10:13:03 -0700

    Add auth tests for #new action JobPosts

--- spec/controllers/job_posts_controller_spec.rb
+++ spec/controllers/job_posts_controller_spec.rb
@@ -1,36 +1,55 @@
 require "rails_helper"
 
 RSpec.describe JobPostsController, type: :controller do 
-   describe "#new" do 
-    it "renders the new template" do 
-        # GIVEN
-        # Defaults
-    
-        # WHEN
-        # Making a GET request to the new action
-        get(:new)
-        # THEN
-        # The 'response' object contains the rendered template 'new'
-        # The response object is available inside any controller. It 
-        # is similar to 'response' available in Express middleware,
-        # however, we rarely interact with it in Rails. RSpec makes
-        # it available when testing, in order to verify its contents.
-    
-        # Here we verify with the 'render_template' matcher that it
-        # contains the right rendered template.
-        expect(response).to(render_template(:new))
+    def current_user 
+        @current_user ||= FactoryBot.create(:user)
     end
+   describe "#new" do
+    context "without signed in user" do 
+        it "redirects the user to session new" do 
+            get :new 
+            expect(response).to redirect_to(new_session_path)
+        end
+        it "sets a danger flash message" do 
+            get :new 
+            expect(flash[:danger]).to be 
+        end
+    end
+    context "with signed in user" do 
+        before do 
+            session[:user_id] = current_user.id 
+        end
 
-    it "sets an instance variable with a new job post" do 
-        get(:new)
-        # assign(:job_post) - returns the value of an instance 
-        # variable @job_post from the instance of our 
-        # JobPostsController.
-        # Only available if the gem 'rails-controller-testing' is 
-        # added.
-        expect(assigns(:job_post)).to(be_a_new(JobPost))
-        # The above matcher (be_a_new) will verify that the expected
-        # value is a new instance of the JobPost model 
+        it "renders the new template" do 
+            # GIVEN
+            # Defaults
+        
+            # WHEN
+            # Making a GET request to the new action
+            get(:new)
+            # THEN
+            # The 'response' object contains the rendered template 'new'
+            # The response object is available inside any controller. It 
+            # is similar to 'response' available in Express middleware,
+            # however, we rarely interact with it in Rails. RSpec makes
+            # it available when testing, in order to verify its contents.
+        
+            # Here we verify with the 'render_template' matcher that it
+            # contains the right rendered template.
+            expect(response).to(render_template(:new))
+        end
+
+        it "sets an instance variable with a new job post" do 
+            get(:new)
+            # assign(:job_post) - returns the value of an instance 
+            # variable @job_post from the instance of our 
+            # JobPostsController.
+            # Only available if the gem 'rails-controller-testing' is 
+            # added.
+            expect(assigns(:job_post)).to(be_a_new(JobPost))
+            # The above matcher (be_a_new) will verify that the expected
+            # value is a new instance of the JobPost model 
+        end
     end
    end 
 
